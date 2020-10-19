ff2861cea89e6f688998030c31535aab7467ece2
alfirajessica <57390147+alfirajessica@users.noreply.github.com>
2020-10-19 10:40:09 +0700

    db collection kelas ada yg diubah, lalu penambahan FK

--- phpmyadmin/db_eccistts (3).sql
+++ phpmyadmin/db_eccistts (3).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 07, 2020 at 11:00 AM
+-- Generation Time: Oct 19, 2020 at 05:38 AM
 -- Server version: 10.4.13-MariaDB
--- PHP Version: 7.4.7
+-- PHP Version: 7.4.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,13 +29,13 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `kelas` (
   `id_periode` int(10) NOT NULL,
-  `id_kelas` varchar(10) NOT NULL,
+  `id_kelas` int(10) NOT NULL,
   `level_ecc` varchar(10) NOT NULL,
-  `nama_kelas` varchar(10) NOT NULL,
+  `nama_kelas` char(1) CHARACTER SET latin1 NOT NULL,
   `hari` varchar(10) NOT NULL,
   `jam` varchar(10) NOT NULL,
   `kuota` int(10) NOT NULL,
-  `dosen` varchar(30) NOT NULL,
+  `dosen` varchar(30) DEFAULT NULL,
   `status_kelas` varchar(2) NOT NULL COMMENT '0 - Tidak aktif, 1 - Aktif'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -44,7 +44,18 @@ CREATE TABLE `kelas` (
 --
 
 INSERT INTO `kelas` (`id_periode`, `id_kelas`, `level_ecc`, `nama_kelas`, `hari`, `jam`, `kuota`, `dosen`, `status_kelas`) VALUES
-(1, 'idcoba1', 'Level 1', 'Kelas A', 'Senin', '06.30', 30, 'aaaa', '1');
+(1, 258, 'Level 1', 'A', 'Senin', '06:30', 0, NULL, '0'),
+(1, 259, 'Level 1', 'B', 'Selasa', '06:30', 0, NULL, '0'),
+(1, 260, 'Level 1', 'C', 'Rabu', '06:30', 0, NULL, '0'),
+(1, 261, 'Level 1', 'D', 'Kamis', '06:30', 0, NULL, '0'),
+(1, 262, 'Level 2', 'A', 'Selasa', '06:30', 0, NULL, '0'),
+(1, 263, 'Level 2', 'B', 'Rabu', '06:30', 0, NULL, '0'),
+(1, 264, 'Level 2', 'C', 'Kamis', '06:30', 0, NULL, '0'),
+(1, 265, 'Level 3', 'A', 'Senin', '06:30', 0, NULL, '0'),
+(1, 266, 'Level 3', 'B', 'Selasa', '06:30', 0, NULL, '0'),
+(1, 267, 'Level 3', 'C', 'Kamis', '06:30', 0, NULL, '0'),
+(1, 268, 'Level 3', 'D', 'Jumat', '06:30', 0, NULL, '0'),
+(1, 269, 'Level 4', 'A', 'Selasa', '06:30', 0, NULL, '0');
 
 -- --------------------------------------------------------
 
@@ -70,26 +81,8 @@ CREATE TABLE `mahasiswa` (
   `nama_mhs` varchar(30) NOT NULL,
   `current_level` varchar(10) NOT NULL,
   `nilai_placement` int(10) NOT NULL,
-  `status_mhs` varchar(2) NOT NULL COMMENT '0 - Mhs nonaktif, 1 - mhs aktif ECC',
-  `id_periode` int(11) NOT NULL
+  `status_mhs` varchar(2) NOT NULL COMMENT '0 - Mhs nonaktif, 1 - mhs aktif ECC'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `mahasiswa`
---
-
-INSERT INTO `mahasiswa` (`nrp`, `nama_mhs`, `current_level`, `nilai_placement`, `status_mhs`, `id_periode`) VALUES
-('217180384', 'name 1', 'IV', 45, '1', 1),
-('217180385', 'name 2', 'IV', 46, '1', 1),
-('217180386', 'name 3', 'II', 24, '1', 1),
-('217180387', 'name 4', 'IV', 56, '1', 1),
-('217180388', 'name 5', 'III', 34, '1', 1),
-('217180389', 'name 6', 'IV', 73, '1', 1),
-('217180390', 'name 7', 'IV', 74, '1', 1),
-('217180391', 'name 8', 'III', 34, '1', 1),
-('217180392', 'name 9', 'IV', 57, '1', 1),
-('217180393', 'name 10', 'II', 24, '1', 1),
-('217180394', 'name 11', 'IV', 72, '1', 1);
 
 -- --------------------------------------------------------
 
@@ -159,7 +152,7 @@ CREATE TABLE `temp_nilaiplacement` (
 --
 
 INSERT INTO `temp_nilaiplacement` (`level1`, `level2`, `level3`, `level4`) VALUES
-(20, 30, 40, 50);
+(200, 300, 400, 500);
 
 -- --------------------------------------------------------
 
@@ -182,9 +175,7 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`username`, `password`, `nama`, `level`, `status`) VALUES
 ('217180345', 'kevin', 'kevin', 'dosen', 1),
 ('217180382', '64ec997f263a63e35d3737e47799ebc53f406bcb', 'alfira', 'admin', 1),
-('alfira1', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 'Fira', 'dosen', 1),
-('Arnold1', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 'Arnold', 'admin', 1),
-('arnold2', 'arnold2', 'test', 'dosen', 1);
+('alfira1', 'ce3eaa938d09504bae9458dffb805f2de7c9da4e', 'Fira', 'dosen', 1);
 
 --
 -- Indexes for dumped tables
@@ -194,7 +185,8 @@ INSERT INTO `user` (`username`, `password`, `nama`, `level`, `status`) VALUES
 -- Indexes for table `kelas`
 --
 ALTER TABLE `kelas`
-  ADD PRIMARY KEY (`id_periode`);
+  ADD PRIMARY KEY (`id_kelas`),
+  ADD KEY `fk_dosen` (`dosen`);
 
 --
 -- Indexes for table `mahasiswa`
@@ -215,12 +207,6 @@ ALTER TABLE `periode`
   ADD PRIMARY KEY (`id_periode`);
 
 --
--- Indexes for table `temp_mahasiswa`
---
-ALTER TABLE `temp_mahasiswa`
-  ADD PRIMARY KEY (`nrp`);
-
---
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -234,13 +220,23 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `kelas`
 --
 ALTER TABLE `kelas`
-  MODIFY `id_periode` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_kelas` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=270;
 
 --
 -- AUTO_INCREMENT for table `periode`
 --
 ALTER TABLE `periode`
   MODIFY `id_periode` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD CONSTRAINT `fk_dosen` FOREIGN KEY (`dosen`) REFERENCES `user` (`username`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
