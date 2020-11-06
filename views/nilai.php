c8f0ab74f20eb0d888d80826c80881571dd04954
Arnold1401 <Arnold.pramudita.wewe@gmail.com>
2020-11-06 13:17:50 +0700

    nilai

--- views/nilai.php
+++ views/nilai.php
@@ -1,124 +1,150 @@
-
 <!DOCTYPE html>
 <html>
 
 <!-- head -->
-<?php require_once("head.php"); 
-$arr=unserialize($_SESSION["user"]); 
-        
+<?php require_once("head.php");
+$arr = unserialize($_SESSION["user"]);
+
 $level = $arr->get_level();
 
 ?>
 
 <body>
-  
-<?php require_once("sidenav.php"); ?>
+
+    <?php require_once("sidenav.php"); ?>
 
     <!-- ini content -->
     <!-- container-fluid -->
-    <div class="container-fluid mt--6">
+    <div class="container-fluid mt-6">
         <!-- row -->
         <div class="row">
-            <div class="col-md-12"> <!-- col-md-12 -->
+            <div class="col-md-12">
+                <!-- col-md-12 -->
                 <div class="card">
-                    <?php 
-                    if($level == "admin")
-                    { require("admin-views/admin_nilai.php"); }
-                    else if ($level == "dosen") {
-                    require("dosen-views/dosen_nilai.php");
+                    <?php
+                    if ($level == "admin") {
+                        require("admin-views/admin_nilai.php");
+                    } else if ($level == "dosen") {
+                        require("dosen-views/dosen_nilai.php");
                     }
                     ?>
                 </div>
             </div> <!-- end of col-md-12 -->
         </div> <!-- end of row -->
 
-      <!-- Footer -->
-      <?php include_once('footer.php') ?>
-      
-    <!-- Modal atur/isi nilai placement  -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-header"> 
-            
-                <h3>Periode/</h3> 
-                <p for="">Kelas</p>                   
-            
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <form role="form">
-                <table class="table table-borderless table-md text-right">
-                    <tbody>
-                        <tr>
-                            <td scope="row">NRP : </td>
-                            <td class="text-left">217180382</td>
-                        </tr>
-                        <tr>
-                            <td scope="row">Nama : </td>
-                            <td class="text-left">Alfira Jessica</td>
-                        </tr>
-                        <tr>
-                            <td scope="row">Jurusan : </td>
-                            <td class="text-left">S1-Sistem Informasi Bisnis</td>
-                        </tr>
-                        <tr>
-                            <td scope="row">Nilai UTS : </td>
-                            <td class="text-left">
-                                <input type="number" class="form-control-sm" name="" id="" aria-describedby="helpId" placeholder=""> 
-                            </td>
-                        </tr>
-                        <tr>
-                            <td scope="row">Nilai UAS : </td>
-                            <td class="text-left">
-                                <input type="number" class="form-control-sm" name="" id="" aria-describedby="helpId" placeholder=""> 
-                            </td>
-                        </tr>
-                        <tr>
-                            <td scope="row">Nilai Akhir : </td>
-                            <td class="text-left">
-                                <input type="number" class="form-control-sm" name="" id="" aria-describedby="helpId" placeholder=""> 
-                            </td>
-                        </tr>
-                        <tr>
-                            <td scope="row">Peringkat : </td>
-                            <td class="text-left">A/B/C/D</td>
-                        </tr>
-                        
-                    </tbody>
-                </table> 
-            </form>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-          </div>
+        <!-- Footer -->
+        <?php include_once('footer.php') ?>
+
+        <!-- Modal atur/isi nilai placement  -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+
+                        <h3>Periode/</h3>
+                        <p for="">Kelas</p>
+
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form role="form">
+                            <table class="table table-borderless table-md text-right" id="example">
+                                <thead>
+                                    <th>#NRP</th>
+                                    <th>Nama Mahasiswa</th>
+                                    <th>UTS</th>
+                                    <th>UAS</th>
+                                    <th>N.Akhir</th>
+                                </thead>
+                                <tr>
+
+                                </tr>
+                                <tbody>
+
+
+                                </tbody>
+                            </table>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    <!-- end of Modal atur dosen/jam/kuota kelas ecc  -->
+        <!-- end of Modal atur dosen/jam/kuota kelas ecc  -->
     </div><!-- container-fluid -->
-  
-  
+
+
 </body>
-<?php include_once('scripts.php')?>
+<?php include_once('scripts.php') ?>
 <script>
-function periode() {
-        $.post("../ajaxes/a_periode.php",
-        {
-            jenis:"get_allperiode",
-        },
-        function(data){
-            $("#periode").html(data);
+    function periode() {
+        $.post("../ajaxes/a_periode.php", {
+                jenis: "get_allperiode",
+            },
+            function(data) {
+                $("#periode").html(data);
             });
     }
+    datatable_lihatsemuamahasiswa();
+    periode();
 
-    $(document).ready(function(){
 
-      periode();
-   });    
+
+
+    function datatable_lihatsemuamahasiswa() {
+        //datatable list barang
+        var idperiode=$("#periode").val();
+        var idkelas=$("#kelas").val();
+        var table = "";
+        table = $('#example').DataTable({
+            dom: 'Bfrtip',
+            "processing": true,
+            "serverSide": true,
+            "bInfo": false,
+            dom: "<'myfilter'f><'mylength'l>t",
+            "pagingType": "numbers",
+            "ordering": true, //set true agar bisa di sorting
+            "order": [
+                [0, 'asc']
+            ], //default sortingnya berdasarkan kolom, field ke 0 paling pertama
+            "ajax": {
+                "url": "../datatables/dosen-datatable/kelas_aktifdt.php",
+                "type": "POST",
+                "idperiode":idperiode,
+                "idkelas":idkelas,
+            },
+            "deferRender": true,
+            "aLengthMenu": [
+                [10, 20, 50],
+                [10, 20, 50]
+            ], //combobox limit
+            "columns": [{
+                    "data": "id_kelas"
+                }
+                // {
+                //     "data": "nrp"
+                // },
+                // {
+                //     "data": "nama"
+                // },
+                // {
+                //     "data": "uts",
+                // },
+                // {
+                //     "data": "uas",
+                // }, 
+                // {
+                //     "data": "na",
+                // }
+
+            ],
+        });
+    }
 </script>
 
 </html>
