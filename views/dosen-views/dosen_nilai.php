e2770c3e56ee5609eea3642b9e8b4ba3aafbbe34
Arnold1401 <Arnold.pramudita.wewe@gmail.com>
2020-11-14 13:45:18 +0700

    commit

--- views/dosen-views/dosen_nilai.php
+++ views/dosen-views/dosen_nilai.php
@@ -70,8 +70,6 @@
     }
 
 
-    
-
     function isikelas() {
         $.post("../ajaxes/a_dos_nilai.php", {
                 jenis: "get_kelasdos",
@@ -85,81 +83,6 @@
 
     function datatable_lihatsemuamahasiswa() {
         //datatable list barang
-<<<<<<< HEAD
-    var periode = $("#periode").val();
-    var table = "";
-<<<<<<< Updated upstream
-    table = $('#example').DataTable({
-        destroy:true,
-        "processing":true,
-            "language": {
-            "lengthMenu": "Tampilkan MENU data per Halaman",
-            "zeroRecords": "Maaf Data yang dicari tidak ada",
-            "info": "Tampilkan data PAGE dari _PAGES_",
-            "infoEmpty": "Tidak ada data",
-            "infoFiltered": "(filtered from MAX total records)",
-=======
-    table = $('#table_tempmhs').DataTable({
-        destroy:true,
-        "processing":true,
-            "language": {
-            "lengthMenu": "Tampilkan _MENU_ data per Halaman",
-            "zeroRecords": "Maaf Data yang dicari tidak ada",
-            "info": "Tampilkan data _PAGE_ dari _PAGES_",
-            "infoEmpty": "Tidak ada data",
-            "infoFiltered": "(filtered from _MAX_ total records)",
->>>>>>> Stashed changes
-            "search":"Cari",
-            "paginate": {
-                "first":      "Pertama",
-                "last":       "terakhir",
-                "next":       "Selanjutnya",
-                "previous":   "Sebelumnya"
-                },
-            },
-        "serverSide":true,
-        "ordering":true, //set true agar bisa di sorting
-        "order": [[0, 'asc']], //default sortingnya berdasarkan kolom, field ke 0 paling pertama
-        "ajax": {
-<<<<<<< Updated upstream
-            "url": "../datatables/dosen-datatable/dt_nilaidosen.php",
-=======
-            "url": "../datatables/dosen-datatable/dosen_nilaidt.php",
->>>>>>> Stashed changes
-            "type": "POST",
-            "data":{"periode":periode},
-        },
-        "deferRender": true,
-        "aLengthMenu": [
-            [10, 20, 50],
-            [10, 20, 50]
-        ], //combobox limit
-        "columns": [
-
-            { "data": "nrp" },
-<<<<<<< Updated upstream
-            { "data": "nama" },
-            { "data": "uts" },
-            { "data": "uas" },
-            { "data": "na" }
-            
-=======
-            { "data": "nilai_uts" },
-            { "data": "nilai_uas" },
-            { "data":"nilai_akhir",
-                
-            },
-            {
-                "data": "grade",
-                
-            },
->>>>>>> Stashed changes
-            
-        ],
-        
-        
-    });
-=======
         var periode = $("#periode").val();
         var kelas = $("#kelas").val();
         var table = "";
@@ -221,6 +144,5 @@
 
 
         });
->>>>>>> 44d1c80555fd0bcf5238f1c639c4e73e2594f061
     }
 </script>
