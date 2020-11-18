645ba45ea19b47c0cfa1e5c685f9702649537031
Arnold1401 <Arnold.pramudita.wewe@gmail.com>
2020-11-18 14:31:41 +0700

    nilai

--- ajaxes/a_nilai.php
+++ ajaxes/a_nilai.php
@@ -33,6 +33,35 @@ if($_POST["jenis"]=="uts"){
         }
     }
      echo "success";
+}else if($_POST["jenis"]=="uas"){
+    $conn = getConn();
+    $uploadfile = $_FILES['file']['tmp_name'];
+    $objExcel = PHPExcel_IOFactory::load($uploadfile);
+    
+    $turncateres = mysqli_query($conn, $turncateqry);
+
+    //get data from excel
+    foreach ($objExcel->getWorksheetIterator() as $worksheet) {
+        $highestrow = $worksheet->getHighestRow();
+
+        for ($row = 0; $row <= $highestrow; $row++) {
+            if ($row >1) {
+                $nrp = $worksheet->getCellByColumnAndRow(0, $row)->getValue();
+                $nama = ucwords($worksheet->getCellByColumnAndRow(1, $row)->getValue());
+                $nilai = $worksheet->getCellByColumnAndRow(2, $row)->getValue();
+                
+                if ($nrp != '') {
+                    
+                    $insert = "INSERT INTO `nilai`(`id_nilai`, `nrp`, `nilai_uts`, `nilai_uas`, `nilai_akhir`, `grade`) VALUES ('0','$nrp','','$nilai','','') ";
+
+                    $insertres = mysqli_query($conn, $insert);
+                }
+            } else {
+                // ini berati headernya di $row=1 
+            }
+        }
+    }
+     echo "success";
 }
     
 ?>
