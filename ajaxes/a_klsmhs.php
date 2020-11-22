8f41b76b6b10c2589e28300cad7209a8af7953a8
alfirajessica <57390147+alfirajessica@users.noreply.github.com>
2020-11-22 14:11:24 +0700

    mahasiswa detail

--- ajaxes/a_klsmhs.php
+++ ajaxes/a_klsmhs.php
@@ -5,52 +5,46 @@ $conn=getConn();
 if($_POST["jenis"]=="cek_dataditable_mahasiswa"){
     $idperiode = $_POST["idperiode"];
     $ket = "";
-    
-    //$sql = 
+
     $sql1 = "select * from tempkelas_mhs where id_periode='$idperiode'";
-    if ($result = mysqli_query($conn, $sql1)) {
-        //simpan semua id kelas pada $set
-        for ($setnrp = array (); $row = $result->fetch_assoc(); $setnrp[] = $row['nrp']);
-        print_r($setnrp);
+    $result = $conn->query($sql1);
+    while ($row = $result->fetch_assoc()) {
+        $nrp = $row["nrp"];
+        $level = $row["level_ecc"];
+        $hari = $row["hari"];
+        $ruang = $row["ruang_kode"];
 
-        for ($setlevel = array (); $row = $result->fetch_assoc(); $setlevel[] = $row['level_ecc']);
-        for ($sethari = array (); $row = $result->fetch_assoc(); $sethari[] = $row['hari']);
-        for ($setruang = array (); $row = $result->fetch_assoc(); $setruang[] = $row['ruang_kode']);
-        
-        for ($i=0; $i <count($setnrp) ; $i++) { 
-            
-            $sql = "INSERT INTO kelas_mhs(id_periode,nrp,id_kelas,id_nilai,status_klsmhs) VALUES ('$idperiode','$setnrp[$i]','0','0','1')";
-
-            if ($conn->query($sql)) {
-                $ket = "Berhasil insert ke tabel!";
-            }else {
-                $ket = "Gagal insert !";
-            }
-
-            //get ruangnya dulu
-            $sqlruang = "select * from ruang_kelas where nama_ruang='$setruang[$i]'";
-            if ($result = mysqli_query($conn, $sqlruang)) {
-                for ($setidruang = array (); $row = $result->fetch_assoc(); $setidruang[] = $row['id_ruangkelas']);
-
-                $sqlkelas = "select * from kelas where level_ecc='Level $setlevel[$i]', hari='$sethari[$i]', id_ruangkelas='$setidruang[$i]', id_periode='$idperiode[$i]'";
-                if ($result = mysqli_query($conn, $sqlkelas)) {
-                    for ($setidkelas = array (); $row = $result->fetch_assoc(); $setidkelas[] = $row['id_kelas']);
-
-                    $sqlupdate = "update kelas_mhs set id_kelas='$setidkelas[$i]' where nrp='$setnrp[$i]'";
-
-                if ($conn->query($sqlupdate)) {
-                    $ket = "Berhasil update tabel !";
-                }else {
-                    $ket = "Gagal insert !";
-                }
-                }
-
-            }
-
+        $sql2 = "select id_ruangkelas from ruang_kelas where nama_ruang='$ruang'";
+        $result2 = $conn->query($sql2);
+        while ($row2 = $result2->fetch_assoc()) {
+            $idruang = $row2["id_ruangkelas"];
         }
-        
-    }
 
+        $sql3 = "select * from kelas where level_ecc='Level $level' and hari='$hari' and id_ruangkelas='$idruang'";
+        $result3 = $conn->query($sql3);
+        while ($row3 = $result3->fetch_assoc()) {
+            $idkelas = $row3["id_kelas"];
+        }
+
+        $sqlinsert = "INSERT INTO kelas_mhs(id_klsmhs,id_periode,nrp,id_kelas,id_nilai,status_klsmhs) VALUES (null,'$idperiode','$nrp','$idkelas','0','1')";
+        if ($conn->query($sqlinsert)) {
+            $ket = "Berhasil Aktifkan semua kelas mahasiswa";
+        }else {
+            $ket = "Gagal Aktifkan!";
+        }
+
+        //delete table tempkelas_mhs
+        $deltempkls = "delete from tempkelas_mhs where id_periode='$idperiode'";
+        if ($conn->query($deltempkls)) {
+            $ket = "Berhasil Aktifkan semua kelas mahasiswa";
+        }else {
+            $ket = "Gagal Aktifkan!";
+        }
+
+        
+
+    }
+    
     
     echo $ket;
     $conn->close();
--- views/scripts.php
+++ views/scripts.php
@@ -18,6 +18,7 @@
         <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
         
         <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+        
         <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
         <script src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script>
         <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.bootstrap4.min.js"></script>
